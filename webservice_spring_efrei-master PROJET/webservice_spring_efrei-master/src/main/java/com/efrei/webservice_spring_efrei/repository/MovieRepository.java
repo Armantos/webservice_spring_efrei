736a008fb07fe131cb93dc45843f79ba5b5c0f9e
Paul KANMADOZO <85197169+paulkanm@users.noreply.github.com>
2022-01-25 23:29:48 +0100

    Delete webservice_spring_efrei-master PROJET/webservice_spring_efrei-master directory

--- webservice_spring_efrei-master PROJET/webservice_spring_efrei-master/src/main/java/com/efrei/webservice_spring_efrei/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.efrei.webservice_spring_efrei.repository;
-
-import com.efrei.webservice_spring_efrei.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
-}
--- webservice_spring_efrei-master PROJET/webservice_spring_efrei-master/src/main/java/com/efrei/webservice_spring_efrei/repository/TheatreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.efrei.webservice_spring_efrei.repository;
-
-import com.efrei.webservice_spring_efrei.model.Theatre;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TheatreRepository extends JpaRepository<Theatre, Integer> {
-}
